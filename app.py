6a0c6f1279638bbbb8bfddf74e31e6f105cc5fe9
Marco <marco.baddy04@gmail.com>
2024-11-21 11:18:11 +0100

    Aggiungo a UV streamlit-option-menu

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ st.write(df)
 Tools.banner_canzone_small("7x8dCjCr0x6x2lXKujYD34")
 Tools.banner_canzone_big("7x8dCjCr0x6x2lXKujYD34")
 
+
+# Sidebar con emoji
+st.sidebar.title("Menù")
+scelta = st.sidebar.radio("🌟 Scegli un'opzione:", ["🚀 Opzione 1", "🌈 Opzione 2", "🧩 Opzione 3"])
