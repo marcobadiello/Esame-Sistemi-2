1b3a3c3a40fcd352c58d5e9e409d519df1956921
marcobadiello <marco.baddy04@gmail.com>
2024-11-21 22:38:05 +0100

    Bisogna aggiustare la selezione del periodo in Top

--- pagine/Top.py
+++ pagine/Top.py
@@ -2,15 +2,33 @@ import Tools
 import streamlit as st
 from datetime import datetime
 from Estrattore import df
+
+
+
 def run_top():
+    oggi = datetime.now().date()
+    anno = oggi.year
+    mese = oggi.month
+    giorno = oggi.day
     n = st.slider(
         'Quante canzoni vuoi visualizzare?',
         min_value=1,
         max_value=100,
         value=3
     )
-    start_date = st.date_input("Seleziona la data di inizio", value=datetime(2022, 11, 1))
-    end_date = st.date_input("Seleziona la data di fine", value=datetime(2023, 12, 31))
-    periodo = (start_date,end_date)
+    periodo = (datetime(anno,1,1),datetime(anno,mese,giorno))
+    anno_selezionato = st.selectbox("Seleziona un anno",[i for i in range(anno,anno-11,-1)],index=0)
+    if st.button("Seleziona"):
+        x = 1
+    if st.button("Selezione avanzata"):
+        start_date = st.date_input("Seleziona la data di inizio", value=datetime(anno_selezionato, 1, 1))
+        end_date = st.date_input("Seleziona la data di fine", value=datetime(anno_selezionato, 12, 31))
+        periodo = (start_date,end_date)
+
+
+
+
+    
 
     Tools.stampa_top_n(df,n,periodo)
+    print(periodo)
