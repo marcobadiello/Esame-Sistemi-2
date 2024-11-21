9c18136ca75d3628a52f130b9a866c45bdf5c502
marcobadiello <marco.baddy04@gmail.com>
2024-11-21 01:42:47 +0100

    Test banner in app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from Estrattore import df
+import Tools
 
 st.title("Spotify wrapped statistico")
 st.write(df)
+Tools.banner_canzone_small("7x8dCjCr0x6x2lXKujYD34")
+Tools.banner_canzone_big("7x8dCjCr0x6x2lXKujYD34")
+
