f55c1f038c29d5f0a11d5e6e5c0fc43c20a74367
marcobadiello <marco.baddy04@gmail.com>
2024-11-28 19:36:57 +0100

    Aggiungo informazioni alla home

--- pagine/home.py
+++ pagine/home.py
@@ -2,6 +2,36 @@ import streamlit as st
 from Estrattore import df
 
 def run_home():
-    st.title("Spotify wrapped statistico")
+    col1, col2 = st.columns([1, 3])
+    with col1:
+        st.image("spotify_logo.png", width=100)
+    with col2:
+        st.title("Spotify Wrapped Statistico")
+    
+    st.markdown("## **Descrizione del progetto**")
+    st.markdown("---")
+    st.subheader("Obbiettivi")
+    st.write('''
+Gli obiettivi di questo progetto sono la presentazione dei miei dati personali di Spotify
+            , cercando di avere un riassunto il più chiaro possibile di quelli che sono i miei gusti musicali e di come sono 
+             variati nel corso del tempo.
+''')
+
+    st.subheader("Il dataset")
+    st.write('''
+            Il dataset raccoglie tutte le mie strem eseguite su Spotify.
+''')
+    st.markdown('''
+        - **ts**: Istante temporale in cui è stata eseguita la stream.
+        - **s_played**: Aggiungi altre descrizioni qui.
+        - **master_metadata_track_name**: Titolo della canzone
+        - **master_metadata_album_artist_name**: Titolo dell'artista
+        - **master_metadata_album_album_name**: Titolo dell'album
+        - **spotify_track_uri**: Codice identificativo del brano
+        - **reason_start**: Motivo per cui è iniziata la stream
+        - **reason_end**: Motivo per cui è terminata la stream
+        - **shuffle**: Variabile dicotomica. La stram era o non era n riproduzione casuale
+        - **skipped**: Variabile dicotomica. La canzone è o non è stata skippata.
+    ''')
     if st.button("Mostra dataframe"):
         st.write(df)
