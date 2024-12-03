2c7071a852d808dc5dc5099573a386d6f72e44e4
marcobadiello <marco.baddy04@gmail.com>
2024-12-03 21:35:10 +0100

    voglio urlare

--- Operazioni_preliminary.py
+++ Operazioni_preliminary.py
@@ -3,12 +3,8 @@ from Analisi import time_series
 from Estrattore import df
 import os
 def avvia_script_R():
-        # Verifica se il file R esiste nella directory del progetto
-    script_path = "Script.R"
-    if not os.path.exists(script_path):
-        print(f"Errore: il file '{script_path}' non esiste nella directory corrente.")
-        return
-    subprocess.run(["Rscript", script_path], check=True, capture_output=True, text=True)
+    res = subprocess.call("Rscript Script.r", shell=True)
+    res
 
 def start():
     time_series(df)
