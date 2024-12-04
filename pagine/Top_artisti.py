ccc413aebec07c81ef88f89403d8eea40ceafaee
marcobadiello <marco.baddy04@gmail.com>
2024-12-04 22:21:16 +0100

    Aggiungo il logo spotify e titolo su tutte le pagine

--- pagine/Top_artisti.py
+++ pagine/Top_artisti.py
@@ -6,6 +6,11 @@ from Estrattore import df
 def run_top_artisti():
     oggi = datetime.now().date()
     anno_corrente = oggi.year
+    col1, col2 = st.columns([1, 3])
+    with col1:
+        st.image("spotify_logo.png", width=100)
+    with col2:
+        st.title("Spotify Wrapped Statistico")
 
     # Slider per il numero di canzoni
     n = st.slider(
