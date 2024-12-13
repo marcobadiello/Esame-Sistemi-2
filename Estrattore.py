0e06ba13a446f7d804fdac582de25acafdbecf19
Marco <marco.baddy04@gmail.com>
2024-12-13 21:47:48 +0100

    Aggiungo i commenti al file Estrattore.py

--- Estrattore.py
+++ Estrattore.py
@@ -1,7 +1,16 @@
 import json
 import polars as pl
 from datetime import datetime
-#creo una funzione che "pulisce" i miei dati
+
+'''
+In questo file viene gestita tutta la creazioen del datafarame dei dati.
+I dati forniti da Spotify sono in formato Json e poichè sono tanto grandi
+sono più file.
+Le informzioen sulle varaibili presenti nel dataframe sono visibili sul pdf
+"ReadMeFirst_ExtendedStreamingHistory.pdf"
+'''
+
+#questa funzione trasforma i file li pulisce e restituisce un dataframe
 def data(report=False):
     #converto il file json in una lisa python
     with open('my_spotify_data/data/json0.json', mode='r', encoding='utf-8') as file:
@@ -16,44 +25,53 @@ def data(report=False):
         df4 = pl.read_json(file,infer_schema_length=20000)
     df = pl.concat([df0, df1, df2, df3, df4], how="vertical")
     
-    
-
-    #il dataframe contiene molte informazioni non utili ai fini del progetto, procedo a rimuovere quelle che non mi interessano
-    # df = df.drop('username')
+    #il dataframe contiene molte informazioni non utili ai fini del progetto, 
+    # procedo a rimuovere quelle che non mi interessano
     df = df.drop('platform')
     df = df.drop('conn_country')
     df = df.drop('ip_addr')
-    # df = df.drop('user_agent_decrypted')
     df = df.drop('offline')
     df = df.drop('offline_timestamp')
     df = df.drop('incognito_mode')
-    print("Ho droppato tutto")
 
-    #rimuovo i podcast
+    #rimuovo tutte le righe che riguardano i podcast
     df = df.filter(pl.col('episode_name').is_null())
 
-    #rimuovo le colonne relative ai podcast
+    # ripulisco ulteriormente i podcast (forse questi codici non sono necessari) 
+    # non ricordo sicneramente meglio non toccare nulla
     df = df.drop("episode_name")
     df = df.drop("episode_show_name")
     df = df.drop('spotify_episode_uri')
 
-    # estraggo i codice delle canzoni
+    # estraggo i codice identificativi delle canzoni
     df = df.with_columns(
         pl.col("spotify_track_uri").str.replace("spotify:track:","")
     )
 
-    #converto i millisecondi in secondi
+    #converto i millisecondi in secondi perchè ha più senso
     df = df.with_columns(
         (pl.col('ms_played')/ 1000).alias('ms_played')
     )
 
-    #rinomino la colonna da millisecondi a secondi
+    #rinomino la colonna da millisecondi a secondi 
     df = df.rename({'ms_played': 's_played'})
 
-    #converto in datetime le date
+    #converto in oggetti datetime le date
     df = df.with_columns(pl.col("ts").str.strptime(pl.Datetime, "%Y-%m-%dT%H:%M:%SZ"))
 
     return df
 
-
+# creo il dataframe
 df = data()
+
+
+
+
+''''
+COSE IMPORTANTI DA SAPERE
+
+- SI lo so per leggere i file si poteva fare un ciclo for ma non mi funzionava proprio bene quindi
+ho deciso di fare così
+- La variabile "df" che viene crate è una varibile globale che poi viene 
+importare in praticamente tutti gli altri file
+'''
