3bf59c5bd8803dc3561115a507f6e5eba8181087
Marco Badiello <155573984+marcobadiello@users.noreply.github.com>
2024-12-18 20:30:56 +0100

    Update shuffle.py

--- pagine/shuffle.py
+++ pagine/shuffle.py
@@ -32,7 +32,7 @@ def run_shuffle():
         st.markdown(
     f"""
     <h1 style='text-align: center; color: #00FF00;'>
-        {round(data[3], 3)}%
+        {round(data[3], 3)*100}%
     </h1>
     """,
     unsafe_allow_html=True
@@ -41,7 +41,7 @@ def run_shuffle():
         st.markdown(
     f"""
     <h1 style='text-align: center; color: #FF0000;'>
-        {round(data[4], 3)}%
+        {round(data[4], 3)×100}%
     </h1>
     """,
     unsafe_allow_html=True
