7dd13d9e4d571f5703a637ead24ffe8b7edcffba
Marco <marco.baddy04@gmail.com>
2025-01-01 21:21:33 +0100

    Rimuovo le API per cambiarle

--- credenziali.py
+++ credenziali.py
@@ -1,3 +1,3 @@
-client_id = '17e444d6b076486fae9c8d8679128b1f'
-client_secret = '69d5a6c6a7214d22966580d1effa3d25'
+client_id = 'xxx'
+client_secret = 'xxx'
 redirect_uri = 'http://localhost:8888/callback'
