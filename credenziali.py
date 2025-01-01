d5de5c0fa250e091f8abcf5bddad58efdfccdb4f
Marco <marco.baddy04@gmail.com>
2025-01-01 21:26:22 +0100

    test

--- credenziali.py
+++ credenziali.py
@@ -1,3 +1,3 @@
-client_id = 'xxx'
-client_secret = 'xxx'
+client_id = '351175fb27a24c9cac6d8c8ff72aa138'
+client_secret = 'ffc639aa6b4b433ea0391ded3a047c79'
 redirect_uri = 'http://localhost:8888/callback'
